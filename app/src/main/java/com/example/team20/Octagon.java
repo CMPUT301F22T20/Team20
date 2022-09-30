1ee02d9864c52accffbc3b9a11eaaef5cbc8a133
“Sidharth <sids6501@gmail.com>
2022-09-29 19:47:08 -0600

    Adding Octagon Shape

--- app/src/main/java/com/example/team20/Octagon.java
+++ app/src/main/java/com/example/team20/Octagon.java
@@ -1,17 +1,2 @@
-package com.example.team20;
-
-public class Octagon extends Shape{
-    int side;
-
-    public Octagon(int side) {
-        this.side = side;
-    }
-
-    public int getSide() {
-        return side;
-    }
-
-    public void setSide(int side) {
-        this.side = side;
-    }
+package com.example.team20;public class Octagon {
 }
